985c6af98e23a4ebd5cf1cca3f585673b49ed745
Asbjorn Kjaer <akjaer@riotgames.com>
2012-11-13 14:27:18 -0800

    Adding support for inserting runlist items after another item, similar to "node run_list add"

--- knife_plugins/bulk_runlist_edit.rb
+++ knife_plugins/bulk_runlist_edit.rb
@@ -25,10 +25,21 @@ module BulkRunlistEdit
              :short => '-P',
              :long => '--preflight',
              :boolean => true,
-             :description => 'Dont do anything, just show the nodes that would be changed'
+             :description => 'Just show the changes, don\'t commit'
+
+      option :after,
+             :short => '-a ITEM',
+             :long => '--after ITEM',
+             :description => 'Place the ENTRY after ITEM in the run list'
+
+      option :changes,
+             :short => '-C',
+             :long => '--show-changes',
+             :boolean => true,
+             :description => 'Output the modified run list on STDOUT'
 
       def run
-         if name_args.size != 2
+         if name_args.size < 2
             ui.fatal "Invalid number of arguments, must specify two arguments"
             show_usage
             exit 1
@@ -36,20 +47,34 @@ module BulkRunlistEdit
 
          nodesearch = Chef::Search::Query.new
          query = name_args.shift
-         runlist_items = name_args.shift
+         runlist_items = name_args
 
          if config[:preflight]
             ui.warn "Only showing changes, not committing"
          end
 
          nodesearch.search('node', query) do |node|
-            runlist_items.split(',').each do |rli|
-               node.run_list << rli
+            if config[:after]
+               nlist = []
+               node.run_list.each do |entry|
+                  nlist << entry
+                  if entry == config[:after]
+                     runlist_items.each { |e| nlist << e }
+                  end
+               end
+               node.run_list.reset!(nlist)
+            else
+               runlist_items.each { |e| node.run_list << e }
             end
 
-            ui.msg "#{ui.color(node.name, :cyan)}: #{node.run_list}"
+            # Only show output if we are in explain or preflight mode
+            if config[:changes] or config[:preflight]
+               ui.msg "#{ui.color(node.name, :cyan)}: #{node.run_list}"
+            end
+
             unless config[:preflight]
                node.save
+               ui.msg "#{ui.color(node.name, :cyan)}: Run list saved"
             end
          end
       end
@@ -64,6 +89,12 @@ module BulkRunlistEdit
              :boolean => true,
              :description => 'Dont do anything, just show the nodes that would be changed'
 
+      option :changes,
+             :short => '-C',
+             :long => '--show-changes',
+             :boolean => true,
+             :description => 'Output the modified run list on STDOUT'
+
       def run
          unless name_args.size >= 2
             ui.fatal('Not enough arguments specified')
@@ -72,8 +103,8 @@ module BulkRunlistEdit
          end
 
          nodesearch = Chef::Search::Query.new
-         query = name_args.first
-         runlist_items = name_args[1 .. -1]
+         query = name_args.shift
+         runlist_items = name_args
 
          if config[:preflight]
             ui.warn "Only showing changes, not committing"
@@ -84,9 +115,14 @@ module BulkRunlistEdit
                node.run_list.remove(rli)
             end
 
-            ui.msg "#{ui.color(node.name, :cyan)}: #{node.run_list}"
+            # Only show output if we are in explain or preflight mode
+            if config[:changes] or config[:preflight]
+               ui.msg "#{ui.color(node.name, :cyan)}: #{node.run_list}"
+            end
+
             unless config[:preflight]
                node.save
+               ui.msg "#{ui.color(node.name, :cyan)}: Run list saved"
             end
          end
       end
